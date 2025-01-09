59c7c75c3eaab5c85774f30c61824d92a3e7ac97
nahyoomi <nahyomi2000@gmail.com>
2025-01-09 20:08:11 +0000

    :goal_net:Add catch errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
