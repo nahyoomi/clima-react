e75582570b123e12ff9acf9413c1f1a9c131f06b
nahyoomi <nahyomi2000@gmail.com>
2025-01-10 08:10:53 +0000

    :art:Feat favorites functionality

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import useStore from '../store/useStore';
 import { FaStar } from 'react-icons/fa';
-import useTemperature from '../hooks/useTemperature'; // Importar el custom hook
+import useTemperature from '../hooks/useTemperature';
+import { useAuth0 } from '@auth0/auth0-react'; // Importar useAuth0
+import { toast } from 'react-toastify'; // Importar toast de react-toastify
 
 const WeatherDisplay: React.FC = () => {
   const weatherData = useStore((state) => state.weatherData);
   const addFavorite = useStore((state) => state.addFavorite);
+  const { isAuthenticated } = useAuth0(); // Obtener isAuthenticated y login
 
   // Llamar al hook de manera incondicional con un valor por defecto
   const tempCelsius = weatherData?.main.temp ?? 0;
@@ -13,12 +16,25 @@ const WeatherDisplay: React.FC = () => {
 
   if (!weatherData) return null;
 
+  const handleAddFavorite = () => {
+    if (isAuthenticated) {
+      addFavorite(weatherData.name);
+      toast.success(`${weatherData.name} ha sido añadido a favoritos`);
+    } else {
+      toast.info('Por favor, inicia sesión para agregar a favoritos');
+    }
+  };
+
   return (
     <div className="bg-white shadow-lg rounded p-4 w-full max-w-md">
       <div className="flex justify-between items-center">
         <h2 className="text-xl font-bold">{weatherData.name}</h2>
-        <button onClick={() => addFavorite(weatherData.name)}>
-          <FaStar className="text-yellow-500" />
+        <button onClick={handleAddFavorite}>
+          <FaStar
+            className={`${
+              isAuthenticated ? 'text-yellow-500' : 'text-gray-400'
+            }`}
+          />
         </button>
       </div>
       <div className="flex items-center">
@@ -27,7 +43,9 @@ const WeatherDisplay: React.FC = () => {
           alt="Icono del clima"
         />
         <div>
-          <p className="text-2xl">{temperature.toFixed(1)}°{unit}</p>
+          <p className="text-2xl">
+            {temperature.toFixed(1)}°{unit}
+          </p>
           <p className="capitalize">{weatherData.weather[0].description}</p>
         </div>
       </div>
